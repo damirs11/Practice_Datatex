54c5ccdf28b7b05adbbaf3e2e55ca04707d7815b
damirs11 <damir.calixov1@gmail.com>
2019-06-05 23:38:23 +0500

    Add PersonsList_class for multiple Person_model serialization

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,9 @@
 package com.company;
 
 
-import com.company.Exception.DocumentExistsException;
-
 public class Main {
 
-    public static void main(String[] args) throws DocumentExistsException {
+    public static void main(String[] args) {
 
 
 
