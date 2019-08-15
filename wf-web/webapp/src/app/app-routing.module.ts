16c908e2a50ba22537205b385fdf9c7fdc41f841
damirs11 <damir.calixov1@gmail.com>
2019-08-15 16:32:21 +0500

    some fixes

--- wf-web/webapp/src/app/app-routing.module.ts
+++ wf-web/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {PersonGridComponent} from "./person-grid/person-grid.component";
+import {PersonFormComponent} from './person-form/person-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: PersonGridComponent},
+    {path: 'create', component: PersonFormComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
